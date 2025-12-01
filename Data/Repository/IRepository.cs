3872bdee3dcac5c287160cbfcaac3fcfba78ae3b
Mardson581 <mardsongames@gmail.com>
2025-12-01 16:03:22 +0000

    Creating models and IRepository

--- Data/Repository/IRepository.cs
+++ Data/Repository/IRepository.cs
@@ -4,9 +4,12 @@ namespace TechInventory.Data.Repository;
 
 public interface IRepository<T> where T : class
 {
-    public Task<Result<T>> Create(T Entity);
-    public Task<Result<T>> Update(T Entity);
-    public Task<Result<T>> Delete(int Id);
-    public Task<Result<T>> Get(int Id);
-    public Task<Result<T>> GetAll();
+    public Task<Result<T>> CreateAsync(T Entity);
+    public Task<Result<T>> UpdateAsync(T Entity);
+    public Task<Result<T>> DeleteAsync(int Id);
+
+    public Task<Result<T>> GetAsync(int Id);
+    public Task<Result<T>> GetByNameAsync(string Name);
+    public Task<IEnumerable<Device>> GetAllAsync();
+    public Task<IEnumerable<Device>> GetAllByTypeAsync(DeviceType Type);
 }

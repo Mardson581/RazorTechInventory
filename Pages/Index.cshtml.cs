8066ad0938bb2aa667a5ad7d6a4b1c45345f6dd0
Mardson581 <mardsongames@gmail.com>
2026-01-02 16:42:31 +0000

    Debugging database connections

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,19 +1,36 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
 
 namespace TechInventory.Pages;
 
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    private readonly IConfigurationManager config;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(ILogger<IndexModel> logger, ConfigurationManager config)
     {
         _logger = logger;
+        this.config = config;
     }
 
     public void OnGet()
     {
-
+        // Crie um teste simples (apagar e retirar config):
+        using var connection = new SqlConnection(config.GetConnectionString("AZURE_SQL_CONNECTIONSTRING"));
+        try
+        {
+            connection.OpenAsync();
+            TempData["Message"] = "Conectado com sucesso!";
+        }
+        catch (Exception ex)
+        {
+            TempData["Message"] = $"Erro: {ex.Message}";
+        }
+        finally
+        {
+            connection.Close();
+        }
     }
 }

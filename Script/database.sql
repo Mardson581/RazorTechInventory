1aca99ba8380129e237a0dd3060b4706b8b82d3a
Mardson581 <mardsongames@gmail.com>
2026-01-06 19:25:33 +0000

    Changing database to MySQL

--- Script/database.sql
+++ Script/database.sql
@@ -1,67 +1,53 @@
-﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
-BEGIN
-    CREATE TABLE [__EFMigrationsHistory] (
-        [MigrationId] nvarchar(150) NOT NULL,
-        [ProductVersion] nvarchar(32) NOT NULL,
-        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
-    );
-END;
-GO
-
-BEGIN TRANSACTION;
-GO
-
-CREATE TABLE [Brands] (
-    [BrandId] int NOT NULL IDENTITY,
-    [Name] nvarchar(40) NOT NULL,
-    CONSTRAINT [PK_Brands] PRIMARY KEY ([BrandId])
+﻿CREATE TABLE IF NOT EXISTS `__EFMigrationsHistory` (
+    `MigrationId` varchar(150) NOT NULL,
+    `ProductVersion` varchar(32) NOT NULL,
+    PRIMARY KEY (`MigrationId`)
 );
-GO
 
-CREATE TABLE [DeviceModels] (
-    [DeviceModelId] int NOT NULL IDENTITY,
-    [Name] nvarchar(50) NOT NULL,
-    [BrandId] int NOT NULL,
-    CONSTRAINT [PK_DeviceModels] PRIMARY KEY ([DeviceModelId]),
-    CONSTRAINT [FK_DeviceModels_Brands_BrandId] FOREIGN KEY ([BrandId]) REFERENCES [Brands] ([BrandId]) ON DELETE NO ACTION
+START TRANSACTION;
+
+CREATE TABLE `Brands` (
+    `BrandId` int NOT NULL AUTO_INCREMENT,
+    `Name` varchar(40) NOT NULL,
+    PRIMARY KEY (`BrandId`)
 );
-GO
 
-CREATE TABLE [Devices] (
-    [DeviceId] int NOT NULL IDENTITY,
-    [Name] nvarchar(50) NOT NULL,
-    [DeviceType] int NOT NULL,
-    [Status] int NOT NULL,
-    [DeviceModelId] int NOT NULL,
-    CONSTRAINT [PK_Devices] PRIMARY KEY ([DeviceId]),
-    CONSTRAINT [FK_Devices_DeviceModels_DeviceModelId] FOREIGN KEY ([DeviceModelId]) REFERENCES [DeviceModels] ([DeviceModelId]) ON DELETE NO ACTION
+CREATE TABLE `DeviceModels` (
+    `DeviceModelId` int NOT NULL AUTO_INCREMENT,
+    `Name` varchar(50) NOT NULL,
+    `BrandId` int NOT NULL,
+    PRIMARY KEY (`DeviceModelId`),
+    CONSTRAINT `FK_DeviceModels_Brands_BrandId` FOREIGN KEY (`BrandId`) REFERENCES `Brands` (`BrandId`) ON DELETE RESTRICT
 );
-GO
 
-CREATE TABLE [MaintenanceRecords] (
-    [MaintenanceRecordId] int NOT NULL IDENTITY,
-    [DeviceId] int NOT NULL,
-    [Data] datetime2 NOT NULL,
-    [Description] nvarchar(500) NOT NULL,
-    [Technician] nvarchar(100) NOT NULL,
-    CONSTRAINT [PK_MaintenanceRecords] PRIMARY KEY ([MaintenanceRecordId]),
-    CONSTRAINT [FK_MaintenanceRecords_Devices_DeviceId] FOREIGN KEY ([DeviceId]) REFERENCES [Devices] ([DeviceId]) ON DELETE CASCADE
+CREATE TABLE `Devices` (
+    `DeviceId` int NOT NULL AUTO_INCREMENT,
+    `Name` varchar(50) NOT NULL,
+    `DeviceType` int NOT NULL,
+    `Status` int NOT NULL,
+    `DeviceModelId` int NOT NULL,
+    PRIMARY KEY (`DeviceId`),
+    CONSTRAINT `FK_Devices_DeviceModels_DeviceModelId` FOREIGN KEY (`DeviceModelId`) REFERENCES `DeviceModels` (`DeviceModelId`) ON DELETE RESTRICT
 );
-GO
 
-CREATE INDEX [IX_DeviceModels_BrandId] ON [DeviceModels] ([BrandId]);
-GO
+CREATE TABLE `MaintenanceRecords` (
+    `MaintenanceRecordId` int NOT NULL AUTO_INCREMENT,
+    `DeviceId` int NOT NULL,
+    `Data` datetime(6) NOT NULL,
+    `Description` varchar(500) NOT NULL,
+    `Technician` varchar(100) NOT NULL,
+    PRIMARY KEY (`MaintenanceRecordId`),
+    CONSTRAINT `FK_MaintenanceRecords_Devices_DeviceId` FOREIGN KEY (`DeviceId`) REFERENCES `Devices` (`DeviceId`) ON DELETE CASCADE
+);
 
-CREATE INDEX [IX_Devices_DeviceModelId] ON [Devices] ([DeviceModelId]);
-GO
+CREATE INDEX `IX_DeviceModels_BrandId` ON `DeviceModels` (`BrandId`);
 
-CREATE INDEX [IX_MaintenanceRecords_DeviceId] ON [MaintenanceRecords] ([DeviceId]);
-GO
+CREATE INDEX `IX_Devices_DeviceModelId` ON `Devices` (`DeviceModelId`);
 
-INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
-VALUES (N'20251230001632_First Migration', N'8.0.3');
-GO
+CREATE INDEX `IX_MaintenanceRecords_DeviceId` ON `MaintenanceRecords` (`DeviceId`);
+
+INSERT INTO `__EFMigrationsHistory` (`MigrationId`, `ProductVersion`)
+VALUES ('20260106192119_Initial MySQL migration', '8.0.3');
 
 COMMIT;
-GO
 

bb4570dd57684c3c1f126b80e6c25bbbc251843c
Mardson581 <mardsongames@gmail.com>
2026-01-08 15:00:44 +0000

    Removing UnitOfWork usage

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 
-builder.Services.AddDbContext<InventoryDbContext>(options =>
+builder.Services.AddDbContextPool<InventoryDbContext>(options =>
     options.UseMySQL(builder.Configuration.GetConnectionString("AZURE_MYSQL_CONNECTIONSTRING"))
 );
 
